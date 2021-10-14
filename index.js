97de9f6ae6def3e92e4d99103a2c39af152b2af0
Andres Liu <andres.liu22@gmail.com>
2021-10-14 02:14:55 -0700

    Set inquirer

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
 const inquirer = require("inquirer");
+const fs = require("fs");
+
+inquirer
+  .prompt([
+    {
+        type: 'input',
+        message: 'Project title: ',
+        name: 'projectTitle',
+    },
+    {
+        type: 'input',
+        message: 'Description: ',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'Installation instructions: ',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'Usage information: ',
+        name: 'usage',
+        },
+    {
+        type: 'input',
+        message: 'Contribution guidelines: ',
+        name: 'contributing',
+    },
+    {
+        type: 'input',
+        message: 'Test instructions: ',
+        name: 'tests',
+    },
+    {
+        type: 'list',
+        message: 'Licence: ',
+        name: 'licence',
+        choices: ["MIT Licence", "GNU GPLv3"]
+    },
+    {
+        type: 'input',
+        message: 'Github username: ',
+        name: 'github',
+    },
+    {
+        type: 'input',
+        message: 'Email address: ',
+        name: 'email',
+    },
+  ])
+  .then((response) => {
+    console.log(response);
+    fs.writeFile("README.md", response, error => {
+        error ? console.log(error) : console.log("README File Created!");
+    })
+  }
+  );
