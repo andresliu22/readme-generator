73701984744acc7b1ba5dd44dc7476521beaa144
Andres Liu <andres.liu22@gmail.com>
2021-10-14 02:41:16 -0700

    Fix readme generator

--- README.md
+++ README.md
@@ -1,21 +1,22 @@
 
-        # README Generator
-        
-        ## Description
-        this is a readme generator for professional purposes
-        ## Table of Contents
+# README Generator
 
-        ### Installation
-        install it as you want what else
-        ### Usage
-        this the usage information
-        ### License
-        MIT License
-        ### Contributing
-        thanks to me Andres Liu
-        ### Tests
-        do tests all the time so we know it works
-        ### Questions
-        [Link to Github] https://github.com/andresliu22
-        If you have any questions, you can send me a mail to miking123@gmail.com.
-    
+## Description
+this is a readme generator
+## Table of Contents
+
+### Installation
+install it now
+### Usage
+use it now
+### License
+Copyright (c) Microsoft Corporation. All rights reserved.
+
+Licensed under the MIT license.
+### Contributing
+contribute now
+### Tests
+no tests here
+### Questions
+* [Link to Github] https://github.com/andresliu22
+* If you have any questions, send me an email to andresliu22@gmail.com. I will try answer your questions as soon as possible.
--- index.js
+++ index.js
@@ -37,7 +37,7 @@ inquirer
         type: 'list',
         message: 'License: ',
         name: 'license',
-        choices: ["MIT License", "GNU GPLv3"]
+        choices: ["MIT", "GPL"]
     },
     {
         type: 'input',
@@ -53,27 +53,30 @@ inquirer
   .then((response) => {
     console.log(response);
 
-    const readme = `
-        # README Generator
-        
-        ## Description
-        ${response.description}
-        ## Table of Contents
+    const readme = 
+`
+# README Generator
 
-        ### Installation
-        ${response.installation}
-        ### Usage
-        ${response.usage}
-        ### License
-        ${response.license}
-        ### Contributing
-        ${response.contributing}
-        ### Tests
-        ${response.tests}
-        ### Questions
-        [Link to Github] https://github.com/${response.github}
-        If you have any questions, you can send me a mail to ${response.email}.
-    `
+## Description
+${response.description}
+## Table of Contents
+
+### Installation
+${response.installation}
+### Usage
+${response.usage}
+### License
+Copyright (c) Microsoft Corporation. All rights reserved.
+
+Licensed under the ${response.license} license.
+### Contributing
+${response.contributing}
+### Tests
+${response.tests}
+### Questions
+* [Link to Github] https://github.com/${response.github}
+* If you have any questions, send me an email to ${response.email}. I will try answer your questions as soon as possible.
+`
     fs.writeFile("README.md", readme, error => {
         error ? console.log(error) : console.log("README File Created!");
     })
